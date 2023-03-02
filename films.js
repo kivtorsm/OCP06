fe6ee8147d32356da7393f000093fd58a840e889
kivtor <112388316+kivtorsm@users.noreply.github.com>
2023-03-02 11:20:12 +0700

    adapt carousel to 4 films width

--- films.js
+++ films.js
@@ -175,3 +175,17 @@ function generateFilms(filmsLists) {
 
 generateFilms(allFilms);
 
+const slidesContainer = document.getElementsByClassName("slides-container");
+const slide = document.querySelector(".film");
+const prevButton = document.getElementById("slide-arrow-prev");
+const nextButton = document.getElementById("slide-arrow-next");
+
+nextButton.addEventListener("click", () => {
+    const slideWidth = slide.clientWidth;
+    slidesContainer.scrollLeft += slideWidth;
+  });
+  
+  prevButton.addEventListener("click", () => {
+    const slideWidth = slide.clientWidth;
+    slidesContainer.scrollLeft -= slideWidth;
+  });
